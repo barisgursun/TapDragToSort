c964212e976588a5e52f9d821be6d5b4abc8bf01
Baris Gursun <baris.gursun@hotmail.com>
2021-06-06 05:00:50 +0300

    sürüklenmesi için ve bırakılan noktaya gitmesi için 2 script eklendi ancak nesneye tıklanınca nesne sahneden gidiyor

---
+++ Assets/Scripts/drag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour
+{
+    public delegate void DragEndedDelegate(drag draggableObject);
+
+    public DragEndedDelegate dragEndedCallback;
+
+    private bool isDrag = false;
+    private Vector3 mouseDragStartPosition;
+    private Vector3 spriteDragStartPosition;
+
+    private void OnMouseDown()
+    {
+        isDrag = true;
+        mouseDragStartPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        spriteDragStartPosition = transform.position;
+
+    }
+    private void OnMouseDrag()
+    {
+        
+        if(isDrag)
+        {
+            transform.localPosition = spriteDragStartPosition + (Camera.main.ScreenToWorldPoint(Input.mousePosition) - mouseDragStartPosition);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDrag = false;
+        dragEndedCallback(this);
+    }
+}
---
+++ Assets/Scripts/snapcontroller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snapcontroller : MonoBehaviour
+{
+
+    public List<Transform> snapPoints;
+    public List<drag> draggbleObjects;
+    public float snapRange = 0.5f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+ 
+    }
+
+    private void OnDragEnded(drag draggable)
+    {
+        float closestDistance = -1;
+        Transform closestSnapPoint = null;
+
+        foreach(Transform snapPoint in snapPoints)
+        {
+            float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+            if(closestSnapPoint == null || currentDistance < closestDistance)
+            {
+                closestSnapPoint = snapPoint;
+                closestDistance = currentDistance;
+            }
+        }
+        if (closestSnapPoint != null && closestDistance <= snapRange)
+        {
+            draggable.transform.localPosition = closestSnapPoint.localPosition;
+        }
+    }
+}
